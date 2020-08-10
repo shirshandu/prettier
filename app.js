beb2a81d8c8b822a647c0ca7f200a9ce0816f974
Shirshandu Bhattacharjee <shirshandu@yahoo.com>
2020-08-10 17:11:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+function hello(
+  shirshandu,
+  rohit,
+  ankur,
+  vaishak,
+  navya,
+  anupriya,
+  akash,
+  sheldon
+) {
+  const abc = 'shirshandu';
+  let aaaa;
+  const shirshandubhattacharjeeankurchatter = 10;
+  for (let i = 0; i <= shirshandubhattacharjeeankurchatter; i++) {
+    aaaa = `${abc}shirshandu`;
+  }
+  return aaaa;
+}
+
+function test(aaa, bbb, ccc) {
+  const abc = 'shirshandu';
+  let aaaa;
+  const shirshandubhattacharjeeankurchatter = 10;
+  for (let incr = 0; incr <= shirshandubhattacharjeeankurchatter; incr++) {
+    aaaa = `${abc}shirshandu`;
+  }
+}
+
+hello('1', '1', '1', '1', '1', '1', '1', '1');
+test('1', '1', '1');
